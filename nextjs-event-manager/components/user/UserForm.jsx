10d920091c81792118f06e0cecc784712cac0447
Paul Bodner <bodner80@gmail.com>
2024-08-22 12:55:03 -0700

    Added tests for PasswordForm component

--- nextjs-event-manager/components/user/UserForm.jsx
+++ nextjs-event-manager/components/user/UserForm.jsx
@@ -2,22 +2,22 @@ export default function UserForm(){
 
   return (
     <form action="/create_user" method="post">
-      <label for="first_name">First Name</label>
+      <label htmlFor="first_name">First Name</label>
       <input type="text" id="first_name" name="first_name" required />
       
-      <label for="last_name">Last Name</label>
+      <label htmlFor="last_name">Last Name</label>
       <input type="text" id="last_name" name="last_name" required />
       
-      <label for="email">Email</label>
+      <label htmlFor="email">Email</label>
       <input type="email" id="email" name="email" required />
       
-      <label for="phone_number">Phone Number (optional)</label>
+      <label htmlFor="phone_number">Phone Number (optional)</label>
       <input type="tel" id="phone_number" name="phone_number" />
 
-      <label for="password">Password</label>
+      <label htmlFor="password">Password</label>
       <input type="password" id="password" name="password" required />
       
-      <label for="confirm_password">Confirm Password</label>
+      <label htmlFor="confirm_password">Confirm Password</label>
       <input type="password" id="confirm_password" name="confirm_password" required />
       
       <input type="submit" value="Create User" />
